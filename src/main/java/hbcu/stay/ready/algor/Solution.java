08dc112510c2be05af064723aecf4f62903d7bdf
hadur2 <hadur@morgan.edu>
2019-08-06 00:02:23 -0400

    done

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,35 @@
 package hbcu.stay.ready.algor;
+import java.util.HashMap;
 
 public class Solution {
 
-    public String countUniqueWords(String input){
-        return null;
+    public String countUniqueWords(String input) {
+
+        String result = "";
+
+        HashMap<String, Integer> newmap = new HashMap<>();
+        String modifiedInput = input.replace(".", " ");
+        String moreModifiedInput = modifiedInput.replaceAll("!", "");
+        String[] output = moreModifiedInput.split("");
+
+        for (String x : output) {
+            if (newmap.containsKey(x)) {
+                newmap.put(x, 1);
+            } else {
+                newmap.put(x, 1);
+            }
+        }
+        result += String.format("The %d unique words are:\n", newmap.size());
+        int size = 0;
+        for (String s : newmap.keySet()) {
+            size++;
+            int value = newmap.get(s);
+            if (size < newmap.size()) {
+                result += String.format("%s (Seen %d)\n", s, value);
+            } else {
+                result += String.format("%s (Seen %d)", s, value);
+            }
+        }
+        return result;
     }
 }
